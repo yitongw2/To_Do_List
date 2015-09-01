1086969df4f8ff47f4547161d3079c61ff98332f
yitongw2 <yitongw2@uci.edu>
2015-08-31 18:28:48 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -6,10 +6,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -17,14 +17,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Reminder[] reminders=new Reminder[4];
         String[] list_item= {"Mon 6/23 - ICS 31 - SSL 270",
         "Tue 6/24 - Do Homework - Home",
                 "Wed 6/25 - Swimming - Pool",
                 "Sat 6/28 - Watch AntMan - AMC Tustin",
                 };
-        ArrayAdapter list_adapter = new ArrayAdapter(this,R.layout.list_text_item,R.id.list_item_text1,list_item);
+        for (int j=0;j<list_item.length;j++){
+            String[] temp=list_item[j].split("-");
+            reminders[j]=new Reminder(temp[1],temp[0],temp[2]);
+        }
+        ArrayList<Reminder> reminders_arraylist=new ArrayList<Reminder>(Arrays.asList(reminders));
+        CustomAdapter c_adapter=new CustomAdapter(this,reminders_arraylist);
         ListView listView = (ListView) findViewById(R.id.list_view1);
-        listView.setAdapter(list_adapter);
+        listView.setAdapter(c_adapter);
     }
 
     @Override
@@ -43,6 +49,11 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
 
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
 
 
         return super.onOptionsItemSelected(item);
