084bc976278655b7c9248f60b7c9d347ddbf779b
yitongw2 <yitongw2@uci.edu>
2015-09-05 17:38:25 -0700

    Update TripleSharedPrefs.java

--- app/src/main/java/com/example/android/todolist/TripleSharedPrefs.java
+++ app/src/main/java/com/example/android/todolist/TripleSharedPrefs.java
@@ -2,8 +2,10 @@ package com.example.android.todolist;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Tony on 9/2/15.
@@ -15,11 +17,11 @@ public class TripleSharedPrefs {
     SharedPreferences sharedPref_key;
     int size;
     public TripleSharedPrefs(Context context, String activity_name){
+        sharedPref_key=context.getSharedPreferences(activity_name+"_key", Context.MODE_PRIVATE);
         sharedPref_content=context.getSharedPreferences(activity_name+"_content", Context.MODE_PRIVATE);
         sharedPref_time=context.getSharedPreferences(activity_name+"_time", Context.MODE_PRIVATE);
-        sharedPref_location=context.getSharedPreferences(activity_name+"_location", Context.MODE_PRIVATE);
-        sharedPref_key=context.getSharedPreferences(activity_name+"_key", Context.MODE_PRIVATE);
-        this.context=context;
+        sharedPref_location=context.getSharedPreferences(activity_name + "_location", Context.MODE_PRIVATE);
+        size=sharedPref_key.getAll().size();
     }
     protected ArrayList<Reminder> getAll(){
         ArrayList<Reminder> result=new ArrayList<Reminder>();
@@ -28,32 +30,74 @@ public class TripleSharedPrefs {
         }
         return result;
     }
-    protected void add(String[] str){
+    protected void edit(String key, String[] str){
+        addContent(key, str[0]);
+        addTime(key, str[1]);
+        addLocation(key,str[2]);
+    }
+    protected int add(String[] str){
         String new_key=addKey();
-        addContent(new_key,str[0]);
-        addTime(new_key,str[1]);
-        addLocation(new_key,str[2]);
+        addContent(new_key, str[0]);
+        addTime(new_key, str[1]);
+        addLocation(new_key, str[2]);
+        return Integer.parseInt(new_key);
+    }
+    protected int generateNewKey(){
+        Random rand=new Random();
+        int num=rand.nextInt((100-0)+1);
+        if (sharedPref_key.contains(Integer.toString(num)))
+            return generateNewKey();
+        else
+            return num;
     }
     private String addKey(){
         SharedPreferences.Editor key_editor=sharedPref_key.edit();
-        String new_key=Integer.toString(sharedPref_key.getAll().size()+1);
+        String new_key=Integer.toString(generateNewKey());
         key_editor.putString(new_key,new_key);
-        key_editor.commit();
+        key_editor.apply();
         return new_key;
     }
     private void addContent(String key, String c){
         SharedPreferences.Editor content_editor=sharedPref_content.edit();
-        content_editor.putString(key,c);
-        content_editor.commit();
+        content_editor.putString(key, c);
+        content_editor.apply();
     }
     private void addTime(String key, String t){
         SharedPreferences.Editor time_editor=sharedPref_time.edit();
         time_editor.putString(key,t);
-        time_editor.commit();
+        time_editor.apply();
     }
     private  void addLocation(String key, String l){
         SharedPreferences.Editor location_editor=sharedPref_location.edit();
-        location_editor.putString(key,l);
-        location_editor.commit();
+        location_editor.putString(key, l);
+        location_editor.apply();
+    }
+    protected int remove(String key){
+        removeKey(key);
+        removeContent(key);
+        removeTime(key);
+        removeLocation(key);
+        size-=1;
+        return size;
+    }
+    private void removeKey(String key){
+        SharedPreferences.Editor key_editor=sharedPref_key.edit();
+        key_editor.remove(key);
+        key_editor.apply();
+    }
+    private void removeContent(String key){
+        SharedPreferences.Editor content_editor=sharedPref_content.edit();
+        content_editor.remove(key);
+        content_editor.apply();
+    }
+    private void removeTime(String key){
+        SharedPreferences.Editor time_editor=sharedPref_time.edit();
+        time_editor.remove(key);
+        time_editor.apply();
+    }
+    private void removeLocation(String key){
+        SharedPreferences.Editor location_editor=sharedPref_location.edit();
+        location_editor.remove(key);
+        location_editor.apply();
     }
 }
