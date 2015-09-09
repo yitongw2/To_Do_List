9214dc88e5e63ceb30f728f232e394c2f299ff5a
yitongw2 <yitongw2@uci.edu>
2015-09-09 00:40:04 -0700

    Update Reminder.java

--- app/src/main/java/com/example/android/todolist/Reminder.java
+++ app/src/main/java/com/example/android/todolist/Reminder.java
@@ -8,13 +8,15 @@ public class Reminder {
     private String time;
     private String location;
     private String content;
-    protected Reminder(String c, String t, String l){
+    private int id;
+    protected Reminder(String c, String t, String l, String i){
         time=t;
         location=l;
         content=c;
+        id=Integer.parseInt(i);
     }
     protected Reminder(){
-        this(null,null,null);
+        this(null,null,null,null);
     }
     protected String getTime(){
         return time;
@@ -25,6 +27,7 @@ public class Reminder {
     protected String getContent(){
         return content;
     }
+    protected int getId(){return id;}
     protected void setTime(String t){
         time=t;
     }
