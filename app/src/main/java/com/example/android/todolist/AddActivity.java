69b09d7a17c6887da001685d374a657ed1379e12
yitongw2 <yitongw2@uci.edu>
2015-09-09 00:38:04 -0700

    Update AddActivity.java

--- app/src/main/java/com/example/android/todolist/AddActivity.java
+++ app/src/main/java/com/example/android/todolist/AddActivity.java
@@ -1,18 +1,21 @@
 package com.example.android.todolist;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
-    String time;
-    String location;
-    String content;
-    int id=-1;
+    EditText editText_time;
+    EditText editText_location;
+    EditText editText_content;
+    private int id = -1;
     private final String key="id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +24,33 @@ public class AddActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_add);
     }
-    public void onSaveInstanceState(Bundle savedInstanceState){
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.getInt(key,id);
-    }
-    protected void onPause(){
+
+    protected void onPause() {
         super.onPause();
-        EditText editText_time=(EditText) findViewById(R.id.activity_add_time);
-        EditText editText_location=(EditText) findViewById(R.id.activity_add_location);
-        EditText editText_content=(EditText) findViewById(R.id.activity_add_content);
-        time=editText_time.getText().toString();
-        location=editText_location.getText().toString();
-        content=editText_content.getText().toString();
-        new WriteToTPF().execute();
+        String time = editText_time.getText().toString();
+        String location = editText_location.getText().toString();
+        String content = editText_content.getText().toString();
+        new WriteToTPF().execute(content,time,location,Integer.toString(id));
+        editText_content=null;
+        editText_time=null;
+        editText_location=null;
     }
-    protected void onStop(){
+    protected void onResume(){
+        super.onResume();
+        editText_time = (EditText) findViewById(R.id.activity_add_time);
+        editText_location = (EditText) findViewById(R.id.activity_add_location);
+        editText_content = (EditText) findViewById(R.id.activity_add_content);
+        if (getIntent().hasExtra(key)){
+            TripleSharedPrefs tsf = new TripleSharedPrefs(AddActivity.this.getApplicationContext(), "Main_Activity");
+            Intent intent=getIntent();
+            id=intent.getIntExtra(key,-1);
+            Reminder re=tsf.get(id);
+            editText_content.setText(re.getContent());
+            editText_time.setText(re.getTime());
+            editText_location.setText(re.getLocation());
+        }
+    }
+    protected void onStop() {
         super.onStop();
     }
 
@@ -57,24 +72,32 @@ public class AddActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-    private class WriteToTPF extends AsyncTask<Void,Void,Void>{
-        protected Void doInBackground(Void... voids){
+
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(key,id);
+    }
+    public void deleteReminder(View view){
+        TripleSharedPrefs tsf = new TripleSharedPrefs(AddActivity.this.getApplicationContext(), "Main_Activity");
+        tsf.remove(Integer.toString(id));
+        finish();
+    }
+    private class WriteToTPF extends AsyncTask<String, Void, Void> {
+        protected Void doInBackground(String... strings) {
+            String content=strings[0];
+            String time=strings[1];
+            String location=strings[2];
             TripleSharedPrefs tsf = new TripleSharedPrefs(AddActivity.this.getApplicationContext(), "Main_Activity");
-            String[] s={content,time,location};
-            if (id>=0 && content.isEmpty() && time.isEmpty() && location.isEmpty()){
+            String[] s = {content, time, location,Integer.toString(id)};
+            if (id >= 0 && content.isEmpty() && time.isEmpty() && location.isEmpty()) {
                 tsf.remove(Integer.toString(id));
-            }
-            else if (id>=0 && !content.isEmpty() && !time.isEmpty() && !location.isEmpty()) {
-                tsf.edit(Integer.toString(id),s);
-            }
-            else if (id<0 && !content.isEmpty() && !time.isEmpty() && !location.isEmpty())
-                id=tsf.add(s);
+            } else if (id >= 0 && (!content.isEmpty() || !time.isEmpty() || !location.isEmpty())) {
+                tsf.edit(Integer.toString(id), s);
+            } else if (id < 0 && (!content.isEmpty() || !time.isEmpty() || !location.isEmpty()))
+                id = tsf.add(s);
             return null;
         }
-
     }
 }
-
