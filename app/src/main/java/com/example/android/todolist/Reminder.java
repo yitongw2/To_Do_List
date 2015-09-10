a24edbdb27eca711753edd1c167b7abba18443ac
yitongw2 <yitongw2@uci.edu>
2015-09-10 10:50:30 -0700

    Update Reminder.java

--- app/src/main/java/com/example/android/todolist/Reminder.java
+++ app/src/main/java/com/example/android/todolist/Reminder.java
@@ -1,6 +1,8 @@
 package com.example.android.todolist;
 
 
+import java.util.Date;
+
 /**
  * Created by Tony on 8/31/15.
  */
@@ -8,15 +10,17 @@ public class Reminder {
     private String time;
     private String location;
     private String content;
+    Date modifiedDate;
     private int id;
-    protected Reminder(String c, String t, String l, String i){
+    protected Reminder(String c, String t, String l, String i, Long m){
         time=t;
         location=l;
         content=c;
         id=Integer.parseInt(i);
+        modifiedDate=new Date(m);
     }
     protected Reminder(){
-        this(null,null,null,null);
+        this(null,null,null,null,null);
     }
     protected String getTime(){
         return time;
@@ -28,6 +32,8 @@ public class Reminder {
         return content;
     }
     protected int getId(){return id;}
+    protected String getLastModifiedDate(){return modifiedDate.toString();}
+    protected void updateModifiedDate(){modifiedDate=new Date();}
     protected void setTime(String t){
         time=t;
     }
