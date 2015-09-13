634036360c425c2b0e240700d51c9801a25d946d
yitongw2 <yitongw2@uci.edu>
2015-09-12 19:17:40 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,44 +1,35 @@
 package com.example.android.todolist;
 
-import android.app.ProgressDialog;
-import android.content.Context;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    CustomAdapter customAdapter;
+public class MainActivity extends AppCompatActivity implements ToDoListFragment.OnItemSelectedListener{
+
+    public final static String key="id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (findViewById(R.id.fragment_container)!=null) {
+            if (savedInstanceState!=null)
+                return;
+            ToDoListFragment listFragment = new ToDoListFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, listFragment).commit();
+        }
     }
-    protected void onStart(){
-        super.onStart();
-    }
-    protected void onResume(){
-        super.onResume();
-        new LoadPrefFile().execute(this);
-    }
-    protected void onRestart(){
-        super.onRestart();
-    }
-    protected void onPause(){
-        super.onPause();
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
@@ -46,46 +37,17 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
-        switch(item.getItemId()){
-            case R.id.action_settings:
-                return true;
-            case R.id.action_add:
-                startAddActivity();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        int id = item.getItemId();
 
-    }
-    private class LoadPrefFile extends AsyncTask<Context,Void,CustomAdapter> {
-        ProgressDialog dialog;
-        protected void onPreExecute(){
-            dialog=new ProgressDialog(MainActivity.this);
-            dialog.setMessage(getResources().getString(R.string.loading));
-            dialog.show();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        protected CustomAdapter doInBackground(Context... contexts){
-            TripleSharedPrefs tsp=new TripleSharedPrefs(contexts[0].getApplicationContext(),"Main_Activity");
-            ArrayList<Reminder> reminderArrayList=tsp.getAll();
-            customAdapter = new CustomAdapter(MainActivity.this, reminderArrayList);
-            return customAdapter;
-        }
-        protected void onPostExecute(CustomAdapter result){
-            dialog.dismiss();
-            ListView listView=(ListView) findViewById(R.id.list_view1);
-            listView.setAdapter(customAdapter);
-        }
+        return super.onOptionsItemSelected(item);
     }
-    public void startAddActivity(View view){
-        LinearLayout linearLayout=(LinearLayout) view;
-        Intent intent=new Intent(this,AddActivity.class);
-        int id=(Integer) linearLayout.getTag(R.string.id_tag);
-        intent.putExtra("id",id);
-        startActivity(intent);
-    }
-    private void startAddActivity(){
+    public void onAddItem(){
         Intent intent=new Intent(this,AddActivity.class);
         startActivity(intent);
     }
+
 }
